f22aaaf2ca9a3638f4f3e01ed8b3da8e6a30a8f8
NourEdres <nouruni410@gmail.com>
2024-04-18 13:48:13 +0300

    changes

---
+++ constants/index.ts
@@ -0,0 +1,7 @@
+import Colors from "./Colors";
+import Font from "./Font";
+import FontSize from "./FontSize";
+import Layout from "./Layout";
+import Spacing from "./Spacing";
+
+export default { Colors, Font, FontSize, Layout, Spacing }
